34708bac14ca66213074a150511a125ccce4fe7f
Cadenmf24 <cadenmf24@gmail.com>
2024-10-04 00:06:03 -0400

    AHHHHHHHHHHHHHHHH

--- tests/test_library.py
+++ tests/test_library.py
@@ -17,46 +17,43 @@ class TestLibrary(unittest.TestCase):
         self.lib.api.get_ebooks = Mock(return_value = self.books_data)
         self.assertTrue(self.lib.is_ebook('learning python'))
 
+    def test_is_ebook_false(self):
+        self.lib.api.get_ebooks = Mock(return_value = self.books_data)
+        self.assertFalse(self.lib.is_ebook(' '))
+
     def test_get_ebooks_count(self):
         self.lib.api.get_ebooks = Mock(return_value=self.books_data)
+        self.assertEqual(self.lib.get_ebooks_count("learning python"), 8)
         
-        self.assertEqual(self.lib.get_ebooks_count("Among us"), 18)
 
     ############################################################################
     #################################  AUTHORS  ###############################
     ############################################################################
 
-    def test_get_ebook_author(self):
-        self.lib.api.get_ebooks = Mock(return_value=self.books_data)
+    def test_is_book_by_author_true(self):
+        self.lib.api.get_ebooks = Mock(return_value = self.books_data)
+        # print(self.lib.api.get_book_info('learning python'))
+        self.assertTrue(self.lib.is_book_by_author('Learning WatchKit programming', 'Wei-Meng Lee'))
+    
+    def test_is_book_by_author_false(self):
+        self.lib.api.get_ebooks = Mock(return_value = self.books_data)
+        # print(self.lib.api.books_by_author('Wei-Meng Lee'))
 
-
-    def test_no_author(self):
+        self.assertFalse(self.lib.is_book_by_author(' ', 'Wei-Meng Lee'))
             
     def test_get_language_for_book(self):
+        self.lib.api.get_ebooks = Mock(return_value=self.books_data)
+        self.assertEqual(self.lib.get_languages_for_book('learning python'), {'hin', 'por', 'eng', 'ger'})
+        
 
-    def test_no_language_for_book(self)
+    # def test_register_patron(self):
 
-    def test_invalid_language_for_book(self):
+    # def test_is_patron_registered_true(self):
 
-    def test_register_patron(self):
+    # def test_is_patron_registered_false(self):
 
-    def test_register_patron_doesnt_exist(self):
+    # def test_borrow_book(self):
 
-    def test_patron_registered(self):
-    
-    def test_book_borrowed(self):
+    # def test_return_borrowed_book(self):
 
-    def test_book_not_exit_borrowed(self):
-
-    def test_book_borrowed_patron_not_registered(self):
-
-    def test_book_returned(self):
-
-    def test_book_returned_book_not_exist(self):
-
-    def test_is_book_borrowed(self):
-
-
-
-    
-
+    # def test_is_book_borrowed(self):
